4e1a904c838607e5b3a7131040aae84f76c7c77f
igor-lourenco <igor.lourencosantos@gmail.com>
2022-01-14 13:21:56 -0300

    useEffect, useState

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,32 @@
 import axios from "axios";
 import Card from "components/Card";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
-  //forma errada
-  axios.get(`${BASE_URL}/filmes?size=12&page=0`).then((response) => {
-    console.log(response.data);
+  
+  const [pageNumber, setPageNumer] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/filmes?size=12&page=1`).then((response) => {
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumer(data.number);
+    });
+  }, []);
+
+  /*FORMA ERRADA
+  axios.get(`${BASE_URL}/filmes?size=12&page=1`).then((response) => {
+    const data = response.data as MoviePage
+    setPageNumer(data.number);
   });
+  */
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
